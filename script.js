3d0996b447943c58717db4670d5b34650e7906d7
Michael P. Johnson <mpjohnson_michigan@hotmail.com>
2016-05-21 13:55:24 -0400

    Updated Programs

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
-function testWord($scope, $http) {$http.get("http://www.purgomalum.com/service/plain?text=test")
-     .then(function(response) {
+var app = angular.module('test', []);
+app.controller('testWord', function($http) {
+  
+// vulgar
+    $http.get("http://www.purgomalum.com/service/plain?text=ass")
+        .then(function(response) {
             console.log(response);
         });
-}
+
+// Not vulgar
+        $http.get("http://www.purgomalum.com/service/plain?text=test")
+        .then(function(response) {
+            console.log(response);
+        });
+});
